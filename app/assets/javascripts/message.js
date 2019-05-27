685e02d058bad39b351e7866fe7208fa0926ece3
Keisuke-Takagi <k19990410@gmail.com>
2019-05-27 21:09:14 +0900

    last message_id get
    name属性を使って最後のidをjsで取得できたのを確認(このコミットのlast_message_idが正確にとれている)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,6 +23,60 @@ $(function() {
   </div>`
     return html;
     }
+    var buildMessageHTML = function(message) {
+      if (message.content && message.image.url) {
+        //data-idが反映されるようにしている
+        var html = '<div class="message" data-id=' + message.id + '>' +
+          '<div class="upper-message">' +
+            '<div class="upper-message__user-name">' +
+              message.user_name +
+            '</div>' +
+            '<div class="upper-message__date">' +
+              message.created_at +
+            '</div>' +
+          '</div>' +
+          '<div class="lower-message">' +
+            '<p class="lower-message__content">' +
+              message.content +
+            '</p>' +
+            '<img src="' + message.image.url + '" class="lower-message__image" >' +
+          '</div>' +
+        '</div>'
+      } else if (message.content) {
+        //同様に、data-idが反映されるようにしている
+        var html = '<div class="message" data-id=' + message.id + '>' +
+          '<div class="upper-message">' +
+            '<div class="upper-message__user-name">' +
+              message.user_name +
+            '</div>' +
+            '<div class="upper-message__date">' +
+              message.created_at +
+            '</div>' +
+          '</div>' +
+          '<div class="lower-message">' +
+            '<p class="lower-message__content">' +
+              message.content +
+            '</p>' +
+          '</div>' +
+        '</div>'
+      } else if (message.image.url) {
+        //同様に、data-idが反映されるようにしている
+        var html = '<div class="message" data-id=' + message.id + '>' +
+          '<div class="upper-message">' +
+            '<div class="upper-message__user-name">' +
+              message.user_name +
+            '</div>' +
+            '<div class="upper-message__date">' +
+              message.created_at +
+            '</div>' +
+          '</div>' +
+          '<div class="lower-message">' +
+            '<img src="' + message.image.url + '" class="lower-message__image" >' +
+          '</div>' +
+        '</div>'
+      };
+      return html;
+    };
     $(".new_message").on("submit", function(e) {
       e.preventDefault();
       var formData = new FormData(this)
@@ -49,6 +103,33 @@ $(function() {
         $('.messages').reset()
       });
   });
+  $(".current-group_name").on("click", function(e) {
+    e.preventDefault();
+    var last_message_id = $('.message:last').data('id');
+    console.log(last_message_id)
+  })
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  //   $.ajax({
+  //     //ルーティングで設定した通りのURLを指定
+  //     url: '/groups/:group_id/api/messages',
+  //     //ルーティングで設定した通りhttpメソッドをgetに指定
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     //dataオプションでリクエストに値を含める
+  //     data: {id: last_message_id}
+  //   })
+  //   .done(function(messages) {
+  //     var insertHTML = '';
+  //     $.each(messages, function(i, message){
+        
+  //     var html = buildMessageHTML(message)
+  //     $(".messages").append(html)
+  //     })
+  //     console.log('success');
+  //   })
+  //   .fail(function() {
+  //     console.log('error');
+    // });
 });
 
   
