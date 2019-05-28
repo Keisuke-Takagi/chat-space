600f45a24ccbdefbc5c8894956b6746c6f93f11d
Keisuke-Takagi <k19990410@gmail.com>
2019-05-28 18:45:22 +0900

    無駄な改行、テスト用コードの削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -25,7 +25,6 @@ $(function() {
     }
       var buildMessageHTML = function(message) {
           if (message.content && message.image.url) {
-            //data-idが反映されるようにしている
             var html = '<div class="message" data-id=' + message.id + '>' +
               '<div class="upper-info">' +
                 '<div class="upper-info__user">' +
@@ -45,7 +44,6 @@ $(function() {
             $('.messages').animate({
               scrollTop: $('.messages')[0].scrollHeight}, '0');
           } else if (message.content) {
-            //同様に、data-idが反映されるようにしている
             var html = '<div class="message" data-id=' + message.id + '>' +
               '<div class="upper-info">' +
                 '<div class="upper-info__user">' +
@@ -64,7 +62,6 @@ $(function() {
             $('.messages').animate({
               scrollTop: $('.messages')[0].scrollHeight}, '0');
           } else if (message.image.url) {
-            //同様に、data-idが反映されるようにしている
             var html = '<div class="message" data-id=' + message.id + '>' +
               '<div class="upper-info">' +
                 '<div class="upper-info__user">' +
@@ -83,7 +80,6 @@ $(function() {
           };
           return html;
         };
-    
     $(".new_message").on("submit", function(e) {
       e.preventDefault();
       var formData = new FormData(this)
@@ -109,55 +105,16 @@ $(function() {
         $('.messages').reset()
       });
   });
-
-  // $(".current-group_name").on("click", function(e) {
-  //   e.preventDefault();
-  //   var last_message_id = $('.message:last').data('id');
-  //   var $dir = location.href.split("/");  
-  //   var $dir2 = $dir[$dir.length -2];
-  //   var url = "/groups/" + $dir2 + "/api/messages" 
-  //   console.log(url)
-  //   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  //   $.ajax({
-  //     //ルーティングで設定した通りのURLを指定
-  //     url: url,
-  //     //ルーティングで設定した通りhttpメソッドをgetに指定
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     //dataオプションでリクエストに値を含める
-  //     data: {id: last_message_id},
-  //   })
-  //   .done(function(message) {
-  //     console.log(message)
-  //     debugger
-  //     // var insertHTML = '';
-  //     // $.each(message, function(i, message){
-  //     //   var html = buildMessageHTML(message)
-  //     //   $(".messages").append(html)
-  //     // })
-  //     console.log('success');
-  //   })
-  //   .fail(function() {
-  //     debugger
-  //     console.log('error');
-  //   });
-  // });
-
   var reloadMessages = function() {
-    
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data('id')
     var last_message_id = $('.message:last').data('id');
     var $dir = location.href.split("/");  
     var $dir2 = $dir[$dir.length -2];
     var url = "/groups/" + $dir2 + "/api/messages" 
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: url,
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'GET',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id},
     })
     .done(function(messages) {
@@ -167,53 +124,14 @@ $(function() {
         var html = buildMessageHTML(message) + insertHTML
         console.log(html)
         $('.messages').append(html);
-        
       })
      })
     .fail(function() {
       console.log('error');
     });
   };
-
   $(function() {
       setInterval(reloadMessages, 5000);
     });
-  // $(".current-group_name").on("click", function(e) {
-  //   e.preventDefault
-  //   var reloadMessages = function() {
-    
-  //     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  //     last_message_id = $('.message:last').data('id')
-  //     console.log(last_message_id)
-  //     var last_message_id = $('.message:last').data('id');
-  //     var $dir = location.href.split("/");  
-  //     var $dir2 = $dir[$dir.length -2];
-  //     var url = "/groups/" + $dir2 + "/api/messages" 
-  //     $.ajax({
-  //       //ルーティングで設定した通りのURLを指定
-  //       url: url,
-  //       //ルーティングで設定した通りhttpメソッドをgetに指定
-  //       type: 'get',
-  //       dataType: 'json',
-  //       //dataオプションでリクエストに値を含める
-  //       data: {id: last_message_id}
-  //     })
-  //     .done(function(messages) {
-  //       console.log(messages)
-  //       var insertHTML = '';
-  //        $.each(messages, function(index, message) {
-          
-  //         var html = buildMessageHTML(message) + insertHTML
-  //         console.log(html)
-  //         $('.messages').append(html);
-  //       })
-  //      })
-  //     .fail(function() {
-  //       console.log('error');
-  //     });
-  //   };
-  //     setInterval(reloadMessages, 5000);
-  //     location.reload();
-
-  // })
 })
+
