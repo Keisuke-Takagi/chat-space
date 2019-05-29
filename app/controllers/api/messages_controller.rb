bb1bca8ba0dc0d3751b268c49c1e723f12f97ea0
Keisuke-Takagi <k19990410@gmail.com>
2019-05-29 14:28:36 +0900

    DELETE @group
    メッセージのgroup_idから検索することによって全てのメッセージの処理を読み込む回数が減り処理が軽くなった

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
     respond_to do |format|
       format.html 
-      format.json { @messages = @group.messages.where('id > ?',  params[:id])}
+      format.json {@messages = Message.where('id > ?',  params[:id]).where(group_id: params[:group_id])}
     end
   end
 end
